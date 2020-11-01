f0828439dbc1ad53cde77b6dba6dc74744349e12
NazarValko <66695679+NazarValko@users.noreply.github.com>
2020-11-01 12:43:44 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+
+
+$(function() {
+
+    var header = $("#header");
+    var intro = $("#intro");
+    var introH = intro.innerHeight();
+    var scrollPos = $(window).scrollTop();
+    var nav = $("#nav");
+    var navToggle = $("#navToggle");
+
+    checkScroll(scrollPos, introH);
+
+    $(window).on("scroll resize", function() {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
+        if( scrollPos >= introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var elementId = $(this).data('scroll');
+        var elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 700);
+    });
+
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+        nav.toggleClass("show");
+    });
+
+});
+(function () {
+    $('.hamburger-menu').on('click', function() {
+        $('.bar').toggleClass('animate');
+    })
+})();
